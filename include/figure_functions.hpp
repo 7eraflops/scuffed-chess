46931e3b94ae07879663af0433faf0ff40e627e6
Michał Charaszkiewicz <michal.charaszkiewicz@gmail.com>
2023-01-13 19:46:39 +0100

    extern

--- include/figure_functions.hpp
+++ include/figure_functions.hpp
@@ -11,15 +11,15 @@ extern void pawn(char board[8][8], figure_coordinates figures[4], bool *moved);
 
 #ifndef KNIGHT_HPP
 #define KNIGHT_HPP
-void knight(char board[8][8], figure_coordinates figures[4], string move, bool *moved);
+extern void knight(char board[8][8], figure_coordinates figures[4], string move, bool *moved);
 #endif
 
 #ifndef ROOK_HPP
 #define ROOK_HPP
-void rook(char board[8][8], figure_coordinates figures[4], string move, bool *moved);
+extern void rook(char board[8][8], figure_coordinates figures[4], string move, bool *moved);
 #endif
 
 #ifndef KING_HPP
 #define KING_HPP
-void king(char board[8][8], figure_coordinates figures[4], char move, bool *moved);
+extern void king(char board[8][8], figure_coordinates figures[4], char move, bool *moved);
 #endif
--- include/randomizer.hpp
+++ include/randomizer.hpp
@@ -2,5 +2,5 @@
 #define RANDOMIZER_HPP
 #include "figure_coordinates.hpp"
 #include <cstdlib>
-void randomizer(char board[8][8], figure_coordinates figures[4]);
+extern void randomizer(char board[8][8], figure_coordinates figures[4]);
 #endif
