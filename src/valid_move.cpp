ac9c82fbf1440c947bbc3f5a6b64f22dafe6d23c
Michał Charaszkiewicz <michal.charaszkiewicz@gmail.com>
2023-01-24 10:01:57 +0100

    small change

--- src/valid_move.cpp
+++ src/valid_move.cpp
@@ -4,13 +4,11 @@ bool valid_move(char board[8][8], int next_x, int next_y)
     bool is_valid = false;
     bool next_x_valid = (next_x >= 0 && next_x <= 7) ? true : false;
     bool next_y_valid = (next_y >= 0 && next_y <= 7) ? true : false;
-    if (next_x_valid && next_y_valid)
+    bool target_space_empty = (board[next_y][next_x] == ' ') ? true : false;
+    if ((next_x_valid && next_y_valid) && target_space_empty)
     {
-        if (board[next_y][next_x] == ' ')
-        {
-            is_valid = true;
-            return is_valid;
-        }
+        is_valid = true;
+        return is_valid;
     }
     return is_valid;
 }
